e5ca857db9d2f29ff392c33e0a86386f93142ced
lekcik <cultcrib@gmail.com>
2024-10-06 23:51:17 +0200

    usecontextadded

--- websiteBuilder/src/App.jsx
+++ websiteBuilder/src/App.jsx
@@ -1,20 +1,46 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createContext, useContext } from "react";
+import { useState } from "react";
 import Home from "./pages/Home.jsx"
 import Error from "./pages/Error.jsx";
 import './root.css';
 import './app.css';
 
+export const PagesContext = createContext();
+
 function App() {
+  const [pages, setPages] = useState(() => {
+    const savedPages = localStorage.getItem('pages')
+    return savedPages
+      ? JSON.parse(savedPages)
+      : [
+        {
+          name: 'Blank Page',
+          elements: [
+            {
+              type: 'h1',
+              innerHTML: 'Hello! That is a blank page!'
+            },
+            {
+              type: 'p',
+              innerHTML: 'Write something here.'
+            }
+          ]
+        }
+      ]
+  });
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </BrowserRouter>
+      <PagesContext.Provider value={[pages, setPages]}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </BrowserRouter>
+      </PagesContext.Provider>
     </>
   )
 }
