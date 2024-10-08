5e2fbfe7a12f1b0ddda2e78047c76627b54ca887
lekcik <cultcrib@gmail.com>
2024-10-09 00:57:48 +0200

    fix attempt

--- websiteBuilder/src/pages/Builder.jsx
+++ websiteBuilder/src/pages/Builder.jsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 import BuilderNav from "../components/BuilderNav.jsx";
 import AddElem from "../components/AddElem.jsx";
 import PagePrev from "../components/PagePrev.jsx";
+import './builder.css';
 
 function Builder() {
     const [showAddElement, setAddElement] = useState(false);
 
     return(
-        <>
+        <section className="builder">
             <BuilderNav setAddElement={setAddElement} />
             <AddElem showAddElement={showAddElement} setAddElement={setAddElement} />
             <PagePrev />
-        </>
+        </section>
     );
 }
 
