e306384b6efbf24808ddcac74b913df3efe465bc
lekcik <cultcrib@gmail.com>
2024-10-10 19:50:25 +0200

    render logic

--- websiteBuilder/src/components/UI/PagePrev.jsx
+++ websiteBuilder/src/components/UI/PagePrev.jsx
@@ -1,40 +1,37 @@
 import './pagePrev.css';
 import AddElem from './AddElem.jsx';
-import { useRef } from 'react';
+import { useState } from 'react';
 import AddElemHandler from '../Functionality/AddElemHandler.jsx';
 
 function PagePrev({ showAddElement, setAddElement }) {
+    const [pageElements, setPageElements] = useState([
+        {
+            type: 'p',
+            text: 'Halo'
+        },
+        {
+            type: 'p',
+            text: 'Halo 2'
+        },
+        {
+            type: 'p',
+            text: 'Halo 3'
+        },
+        {
+            type: 'p',
+            text: 'Halo 4'
+        }
+    ]);
 
     return (
         <section className='pagePrev'>
-            <AddElem showAddElement={showAddElement} setAddElement={setAddElement} />
+            <AddElem showAddElement={showAddElement} setAddElement={setAddElement} pageElements={pageElements} setPageElements={setPageElements} />
             <section className='pageElements'>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
-                <p>halo</p>
+                {
+                    pageElements.map((data, index) => (
+                        <p key={index}>{data.text}</p>
+                    ))
+                }
             </section>
         </section>
     );
